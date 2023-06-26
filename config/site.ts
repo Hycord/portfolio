e7c8be613ba20c594d6035ea3eebc49d13b703ed
Hycord <hycord@hycordia.com>
2023-06-26 01:25:31 -0500

    initial commit

--- config/site.ts
+++ config/site.ts
@@ -1,9 +1,9 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Next.js",
+  name: "Hycord",
   description:
-    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+    "Portfolio website coming soon.",
   mainNav: [
     {
       title: "Home",
@@ -11,8 +11,7 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
+    twitter: "https://twitter.com/ignhycord",
+    github: "https://github.com/hycord",
   },
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
+  distDir: "build"
 }
 
 export default nextConfig
