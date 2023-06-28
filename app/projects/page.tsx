9ff1c8c5e57b466d7e88975c57f370a86b84d2b7
Hycord <hycord@hycordia.com>
2023-06-28 02:03:48 -0500

    0.12.3
        - ✨ (Bug Fix) > Hide private projects from project page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,17 +4,17 @@ import { useMemo } from "react"
 
 import { allDocuments } from "../../.contentlayer/generated"
 import { ProjectShowcase } from "../../components/ProjectShowcase"
-import { ProjectDesktopLayout } from "../../components/projects/ProjectDesktopLayout"
-import { ProjectMobileLayout } from "../../components/projects/ProjectMobileLayout"
 
 export default function Page() {
   return (
     <>
       <section className="grid max-h-full w-full grid-cols-1 gap-4 overflow-scroll p-4 mobile:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
         {/* <div className="grid max-h-full w-full grid-cols-1 gap-4 overflow-scroll p-4 mobile:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5"> */}
-        {allDocuments.map((doc) => (
-          <ProjectShowcase key={doc.slugAsParams} doc={doc} />
-        ))}
+        {allDocuments
+          .filter((doc) => doc.type == "Project" && doc.public)
+          .map((doc) => (
+            <ProjectShowcase key={doc.slugAsParams} doc={doc} />
+          ))}
         {/* </div> */}
       </section>
     </>
