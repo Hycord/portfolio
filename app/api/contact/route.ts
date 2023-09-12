718e97c3ca7b23f1536443f62c511261a59d4f5f
Hycord <hycord@hycordia.com>
2023-09-11 19:19:26 -0500

    remove bad import

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -4,7 +4,6 @@ import axios from "axios"
 import ipware from "ipware"
 import z, { ZodError } from "zod"
 
-import { ContactFormSubmission } from "../../lib/types/contact"
 
 const schema = z.object({
   firstName: z.string().min(1),
